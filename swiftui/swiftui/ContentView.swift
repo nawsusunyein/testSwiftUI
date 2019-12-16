9c585fdbaa64827dc62263b1b74aba930a554c03
nawsusunyein <nawsusunyein@gmail.com>
2019-12-16 15:53:34 +0630

    build project

---
+++ swiftui/swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui
+//
+//  Created by techfun on 2019/12/16.
+//  Copyright © 2019 Naw Su Su Nyein. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
